3f8724827e43839855a761fc228cbbeda1704ce8
Jonathan Peros <jperos@jenova.games>
2025-09-23 02:25:46 -0700

    Changing gcc version to 2.8.1. Decompile SetVoiceSampleRate

--- src/slps_023.64/system/sound.c
+++ src/slps_023.64/system/sound.c
@@ -1,35 +1,51 @@
 #include "common.h"
+#include "psyq/libspu.h"
 #include "hw.h"
+
+typedef struct {
+    SpuVolume volume;
+    u16 pitch;
+    u16 addr;
+    u16 adsr[2];
+    u16 volumex;
+    u16 loop_addr;
+} SPU_VOICE_REG;
 
 //----------------------------------------------------------------------------------------------------------------------
 void SetVoiceKeyOn( u32 in_KeyOn )
 {
-    SPU_VOICE_KEY_ON_LO = in_KeyOn;
-    SPU_VOICE_KEY_ON_HI = (in_KeyOn >> 0x10);
+    *SPU_VOICE_KEY_ON_LO = in_KeyOn;
+    *SPU_VOICE_KEY_ON_HI = (in_KeyOn >> 0x10);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 void SetVoiceKeyOff( u32 in_KeyOff )
 {
-    SPU_VOICE_KEY_OFF_LO = in_KeyOff;
-    SPU_VOICE_KEY_OFF_HI = (in_KeyOff >> 0x10);
+    *SPU_VOICE_KEY_OFF_LO = in_KeyOff;
+    *SPU_VOICE_KEY_OFF_HI = (in_KeyOff >> 0x10);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 void SetVoiceReverbMode( u32 in_ReverbMode )
 {
-    SPU_VOICE_CHN_REVERB_MODE_LO = in_ReverbMode;
-    SPU_VOICE_CHN_REVERB_MODE_HI = (in_ReverbMode >> 0x10);
+    *SPU_VOICE_CHN_REVERB_MODE_LO = in_ReverbMode;
+    *SPU_VOICE_CHN_REVERB_MODE_HI = (in_ReverbMode >> 0x10);
 }
 
+//----------------------------------------------------------------------------------------------------------------------
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceNoiseMode);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceFmMode);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceVolume);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceSampleRate);
+//----------------------------------------------------------------------------------------------------------------------
+void SetVoiceSampleRate( s32 in_VoiceIndex, s16 in_SampleRate )
+{
+    VOICE_00_ADPCM_SAMPLE_RATE[in_VoiceIndex * (sizeof(SPU_VOICE_REG) / sizeof(u16))] = in_SampleRate;
+}
 
+//----------------------------------------------------------------------------------------------------------------------
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceStartAddr);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/sound", SetVoiceRepeatAddr);
