7eeac5a553e59dc248bf8a408c0146850dd457dd
Jonathan Peros <jperos@jenova.games>
2025-10-26 09:49:09 +0000

    Found another block of libspu functions, throwing in a source file for now

---
+++ src/slps_023.64/psyq/libspu2.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuInitMalloc);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuSetIRQ);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuSetIRQCallback);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", _SpuCallback);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuRead);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuWrite);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", SpuSetTransferMode);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libspu2", func_8004BC64);
