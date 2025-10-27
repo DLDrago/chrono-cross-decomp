153abc6e03af5ea9a45254dd04ea583896f69dd1
Jonathan Peros <jperos@jenova.games>
2025-10-27 06:35:53 +0000

    Identifying Libapi functions

---
+++ src/slps_023.64/psyq/libapi.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", SetRCnt);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", GetRCnt);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", StartRCnt);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", StopRCnt);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", ResetRCnt);
