c3c02e0110b20dcfb2a60e6ffe0a4ec430d37156
Jonathan Peros <jperos@jenova.games>
2025-10-27 08:04:09 +0000

    Identifying libapi splits

--- src/slps_023.64/psyq/libapi.c
+++ src/slps_023.64/psyq/libapi.c
@@ -1,11 +1,23 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", SetRCnt);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", InitHeap);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", GetRCnt);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", FlushCache);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", StartRCnt);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", GPU_cw);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", StopRCnt);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", DeliverEvent);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", ResetRCnt);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", OpenEvent);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", CloseEvent);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", TestEvent);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", EnableEvent);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", UnDeliverEvent);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", EnterCriticalSection);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libapi", ExitCriticalSection);
