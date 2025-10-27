78a0688eb8181ebf650782cd0d276103eedf5dc9
Jonathan Peros <jperos@jenova.games>
2025-10-27 09:29:41 +0000

    Identifying libsn functions

--- src/slps_023.64/psyq/libsn.c
+++ src/slps_023.64/psyq/libsn.c
@@ -1,15 +1,15 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_80028560);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", PCopen);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_80028580);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_80028590);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", PClseek);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_800285B4);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_800285C0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", PCread);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_80028680);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", _SN_read);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", func_80028698);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libsn", __fixunsdfsi);
