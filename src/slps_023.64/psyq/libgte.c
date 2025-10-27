8d928c3c6a16d0eb3ea4132fd6652ae44a2c27d5
Jonathan Peros <jperos@jenova.games>
2025-10-27 09:20:23 +0000

    Identifying libgte functions

--- src/slps_023.64/psyq/libgte.c
+++ src/slps_023.64/psyq/libgte.c
@@ -2,16 +2,9 @@
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", rsin);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", GEO_00_OBJ_2C);
-
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", sin_1);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", GEO_00_OBJ_C4);
-
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", rcos);
-
-void GEO_01_OBJ_98(void) {
-}
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", MatrixNormal_0);
 
@@ -33,166 +26,166 @@ INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", InvSquareRoot);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", VectorNormalS);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800257C0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", VectorNormal);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800257F0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", VectorNormalSS);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025820);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800258E0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", MatrixNormal);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800259D0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverage12);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025A20);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverage0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025A70);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverageShort12);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025AF8);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverageShort0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025B80);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverageByte);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025BE0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LoadAverageCol);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025C60);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", MulMatrix0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025D70);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", CompMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025ED0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ApplyRotMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025F00);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", PushMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80025FA4);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", PopMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026050);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ReadRotMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800260A0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetMulMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800261B0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", MulMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800262C0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ApplyMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026310);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ApplyMatrixSV);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026370);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800263A0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ScaleMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800264E0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetRotMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026510);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetLightMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026540);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetColorMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026570);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetTransMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026590);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetVertex0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800265A0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetVertex1);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800265B0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetVertex2);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800265C0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetVertexTri);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800265E0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetRGBfifo);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800265F4);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetIR123);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026608);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetIR0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026614);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetSZfifo3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026628);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetSZfifo4);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026640);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetSXSYfifo);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026654);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetRii);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026668);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetMAC123);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_8002667C);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetData32);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026688);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetDQA);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026694);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetDQB);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800266A0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800266B0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetBackColor);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800266D0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", SetFarColor);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800266F0);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026710);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026720);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LocalLight);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026744);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", DpqColor);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026760);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColor);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_8002677C);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColor3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800267B8);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColorDpq);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800267DC);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColorDpq3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026824);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColorCol);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026844);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", NormalColorCol3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026888);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ColorDpq);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800268B0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ColorCol);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800268D4);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", AverageSZ3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800268E4);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", AverageSZ4);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026900);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", LightColor);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026928);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", DpqColorLight);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026950);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", DpqColor3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_8002698C);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", Intpl);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800269B0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", Square12);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800269D8);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", Square0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026A00);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", AverageZ3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026A20);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", AverageZ4);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026A44);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", OuterProduct12);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026A9C);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", OuterProduct0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026AF4);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", Lzc);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026B10);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotTransPers);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026B40);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotTransPers3);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026BA0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotTrans);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026BD0);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026C00);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotTransPers4);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026C80);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ColorMatCol);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026CC0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotMatrix);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80026F50);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotMatrixXZY);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800271E0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotMatrixZYX_gte);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800273E0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", RotMatrix_gte);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_800275F0);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", ratan2);
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_80027770);
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", _patch_gte);
 
 INCLUDE_ASM("asm/slps_023.64/nonmatchings/psyq/libgte", func_8002781C);
