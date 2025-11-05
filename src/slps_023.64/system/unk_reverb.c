5c379777ca40ea9bd5ac1b8edc860901014b3b90
Jonathan Peros <jperos@jenova.games>
2025-11-05 07:31:15 +0000

    Decompile SetReverbMode

---
+++ src/slps_023.64/system/unk_reverb.c
@@ -0,0 +1,39 @@
+#include "common.h"
+#include "psyq/libspu.h"
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80050C48);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80050D38);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80050DD4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80050EF0);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80050F84);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80051000);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80051094);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_80051110);
+
+//----------------------------------------------------------------------------------------------------------------------
+void func_80051234(void) {
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void SetReverbMode( s32 in_ReverbMode )
+{
+    s32 currentReverbMode;
+
+    SpuGetReverbModeType( &currentReverbMode );
+    if( currentReverbMode != in_ReverbMode )
+    {
+        SpuSetReverb( SPU_OFF );
+        SpuSetReverbModeType( in_ReverbMode | SPU_REV_MODE_CLEAR_WA );
+        SpuSetReverb( SPU_ON );
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/unk_reverb", func_8005128C);
