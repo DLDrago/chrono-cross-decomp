b7fc5e8fdc3c5910f93ea66b8861425d8c2a5402
Jonathan Peros <jperos@jenova.games>
2026-01-20 23:02:17 +0000

    Decompile SoundVM_AE_DecayRate

--- src/slps_023.64/system/soundVM.c
+++ src/slps_023.64/system/soundVM.c
@@ -275,12 +275,19 @@ void SoundVM_AD_AttackRate( FSoundChannel* in_pChannel, u32 in_VoiceFlags )
 {
     u16 AttackRate = *in_pChannel->ProgramCounter++;
     in_pChannel->VoiceParams.AdsrLower &= ~SOUND_ADSR_ATTACK_RATE_MASK;
-    in_pChannel->VoiceParams.AdsrLower |= AttackRate << 8;
+    in_pChannel->VoiceParams.AdsrLower |= AttackRate << SOUND_ADSR_ATTACK_RATE_SHIFT;
     in_pChannel->VoiceParams.VoiceParamFlags |= (VOICE_PARAM_ADSR_AMODE | VOICE_PARAM_ADSR_AR);
     in_pChannel->UpdateFlags |= 0x01000000;
 }
 
-INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundVM", SoundVM_AE_DecayRate);
+//----------------------------------------------------------------------------------------------------------------------
+void SoundVM_AE_DecayRate( FSoundChannel* in_pChannel, u32 in_VoiceFlags )
+{
+    u16 DecayRate = *in_pChannel->ProgramCounter++;
+    in_pChannel->VoiceParams.AdsrLower &= ~SOUND_ADSR_DECAY_RATE_MASK;
+    in_pChannel->VoiceParams.AdsrLower |= DecayRate << SOUND_ADSR_DECAY_RATE_SHIFT;
+    in_pChannel->VoiceParams.VoiceParamFlags |= VOICE_PARAM_ADSR_DR;
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 void SoundVM_AF_SustainLevel( FSoundChannel* in_pChannel, u32 in_VoiceFlags )
