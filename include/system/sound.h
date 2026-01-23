424166192d21ea67aaaebfd90b40e56a157da619
Jonathan Peros <jperos@jenova.games>
2026-01-23 05:11:47 +0000

    Decompile Sound_FE03_SetMasterReverbSlide

--- include/system/sound.h
+++ include/system/sound.h
@@ -464,7 +464,7 @@ void SoundVM_A0_FinishChannel( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE00_SetTempo( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE01_SetTempoSlide( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE02_80054028( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
-void SoundVM_FE03_80054070( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
+void SoundVM_FE03_SetMasterReverbSlide( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE06_80054118( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE07_80054144( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE0E_8005419c( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
