d0d3fe6ec67fc09c8d2c2fb349a3ceaa3069eb7d
Jonathan Peros <jperos@jenova.games>
2026-01-23 07:29:58 +0000

    Decompile SoundVM_FE0A_ClearInstrument

--- include/system/sound.h
+++ include/system/sound.h
@@ -481,7 +481,7 @@ void SoundVM_A5_SetOctave( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_A6_IncreaseOctave( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_A7_DecreaseOctave( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_A1_LoadInstrument( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
-void SoundVM_FE0A_80054580( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
+void SoundVM_FE0A_ClearInstrument( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_FE14_800545ec( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_B3_ResetAdsr( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
 void SoundVM_C0_ChannelTranspose_Absolute( FSoundChannel* in_pChannel, u32 in_VoiceFlags );
