5bba8f7a72acd66f11e7e1d48286573371bc9031
Jonathan Peros <jperos@jenova.games>
2026-01-29 03:01:58 +0000

    Breaking out and fixing more sound splits

---
+++ src/slps_023.64/system/soundApi.c
@@ -0,0 +1,135 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", InitSound);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", TeardownSound);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", Sound_BindAkaoSfxBlob);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_80049FBC);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_80049FE4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A00C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A03C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A05C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A07C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A0B0);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A0E8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A118);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A168);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A1F8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A234);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A260);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A2C8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A334);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A3B4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A3E8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A410);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A438);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A4A4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A510);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A53C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A570);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A5B4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A5FC);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A628);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A65C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A6A0);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A6E8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A714);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A748);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A78C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A7D4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A808);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A840);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A880);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A8B4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A8EC);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A914);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A944);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A978);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A9A4);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004A9D8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AA14);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AA40);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AA74);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AAB0);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AADC);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AB10);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AB4C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AB6C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AB8C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", IsSpuTransferring);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AC0C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AC2C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AE4C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AE6C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AF00);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AF20);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AF50);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AF88);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004AFC8);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004B04C);
+
+INCLUDE_ASM("asm/slps_023.64/nonmatchings/system/soundApi", func_8004B080);
